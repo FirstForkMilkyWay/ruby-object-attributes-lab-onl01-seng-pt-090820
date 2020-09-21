11f9fafb2e77f984230633d9eabd474a2cd93175
Stuart Fleishman <stuart.a.fleishman@gmail.com>
2020-09-21 16:57:21 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person 
+    def name=(person_name)
+    @this_person_name = person_name
+  end 
+ 
+  def name
+    @this_person_name
+  end
+  
+    def job=(job_name)
+    @this_person_breed = breed_name
+  end
+ 
+  def breed
+    @breed
+  end
+
+end 
